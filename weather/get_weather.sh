59802dbbf31bd2885f67361c729e67cd01f8d951
Jamie Saxon <jsaxon@uchicago.edu>
2016-10-22 16:32:26 -0500

    fixed up the weather scripts -- all set

--- weather/get_weather.sh
+++ weather/get_weather.sh
@@ -1,17 +1,28 @@
 #!/bin/bash 
 
-mkdir -p weather
-cd weather
-for y in 2013 2014 2015 2016; do 
-  for m in $(seq -w 1 12); do 
-    for d in $(seq -w 1 31); do 
-      echo ${y}${m}${d}
-      curl https://www.wunderground.com/history/airport/KMDW/${y}/${m}/${d}/DailyHistory.html?format=0 -s -o ${y}${m}${d}
+# with thanks and apologies to weather underground...
+
+# MDW
+for w in JFK PHL PDX LAX BOS PHX DEN SFO DFW; do 
+
+  mkdir -p ${w}
+  for y in $(seq 2000 2016); do 
+    for m in $(seq -w 1 12); do 
+      for d in $(seq -w 1 31); do 
+        echo ${w} ${y}-${m}-${d}
+        curl https://www.wunderground.com/history/airport/K${w}/${y}/${m}/${d}/DailyHistory.html?format=0 \
+             -s -o ${w}/${y}${m}${d}
+      done
     done
   done
+
+  # cat ${w}/2* | sed "s/<br \/>//" | sort | uniq | sort -k 14 -t"," > weather_${w}.csv
+  # rm ${w}/
+
 done
 
-cd ..
-cat weather/2* | sed "s/<br \/>//" | sort | uniq | sort -k 14 -t"," > weather_2013-2016
-rm -rf weather
+sed -i -e "s/<br \/>//" -e "/^$/d" */*
 
+for f in $(grep "No .*data available" */* | sed "s/:.*//"); do rm $f; done
+for f in $(wc -l */* | grep " 1 " | tr -s ' ' | cut -f3 -d" "); do rm $f; done
+ 
---
+++ weather/merge.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python 
+
+import pandas as pd
+import pytz
+import os  
+
+def merge_weather_data(airport):
+
+    df_list = []
+    for y in range(2000, 2017):
+        for m in range(1, 13):
+            for d in range(1, 32):
+                
+                f = "{}/{:04d}{:02d}{:02d}".format(airport, y, m, d)
+                if not os.path.isfile(f): continue
+                
+                print(f)
+                df_list.append(pd.read_csv(f, index_col = "DateUTC", parse_dates = ['DateUTC']))
+                
+    df = pd.concat(df_list)
+    
+    # Drop a few bogus values
+    df = df[df['TemperatureF'] != -9999.0]
+    df["PrecipitationIn"].fillna(0, inplace = True)
+    
+    df.set_index(df.index.tz_localize(pytz.UTC).tz_convert(pytz.timezone('US/Central')),
+                 inplace = True)
+    
+    df.rename(columns = {"TemperatureF" : "Temperature [F]", "PrecipitationIn" : "Precipitation [In]"})\
+      .to_csv(airport + ".csv")
+
+
+airports = ["PDX", "JFK", "PHL", "LAX", "BOS", "PHX", "DEN", "SFO", "DFW"]
+for a in airports:
+    merge_weather_data(a)
+
